3e2ca5c68dde1990d8cca4c87b76ccd56bf13a1a
Waqar Aslam <waqar.king10@gmail.com>
2024-03-24 21:17:00 +0500

    Fix main page error

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default async function Page({ searchParams }: Props) {
             >
               <div className="hidden lg:block">
                 {/* Product filters */}
-                <`ProductFilters />
+                <ProductFilters/>
               </div>
               {/* Product grid */}
               <ProductGrid products={products} />

9bf749813d71d4a6c9b4e54dfe4ee4c5727f73ff
Waqar Aslam <waqar.king10@gmail.com>
2024-06-16 16:19:37 +0500

    selected product page

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,12 @@ interface Props {
     category? : string
     size? : string
     color? : string
+    search? : string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = 'desc', price, color, category, size } = searchParams
+  const { date = 'desc', price, color, category, size, search } = searchParams
   const priceOrder = price ? `| order(price ${price})` : ""
   const dateOrder = date ? `| order(_CreatedAt ${date})` : ""
   const order = `${priceOrder}${dateOrder}`
@@ -29,7 +30,8 @@ export default async function Page({ searchParams }: Props) {
   const categoryFilter = category ? `&& "${category}" in categories` : ""
   const sizeFilter = size ? `&& "${size}" in sizes` : ""
   const colorFilter = color ? `&& "${color}" in colors` : ""
-  const filter = `*[${productFilter}${categoryFilter}${sizeFilter}${colorFilter}]`
+  const searchFilter = search ? `&& name match "${search}"` : ""
+  const filter = `*[${productFilter}${categoryFilter}${sizeFilter}${colorFilter}${searchFilter}]`
 
 
   const products = await client.fetch<SanityProduct>(
--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -5,16 +5,43 @@ import { SanityProduct } from "@/config/inventory"
 import { ProductGallery } from "@/components/product-gallery"
 import { ProductInfo } from "@/components/product-info"
 
-interface Props {}
+interface Props {
+  params: {
+    slug: string
+  }
+}
 
-export default function Page() {
+export default async function Page( {params} : Props) {
+  const product = await client.fetch<SanityProduct>(
+    groq`*[_type == "product" && slug.current == "${params.slug}"][0] 
+    {
+      _id,
+      "id" : _id,
+      name,
+      sku,
+      images,
+      currency,
+      price,
+      description,
+      "slug": slug.current,
+      _createdAt,
+      sizes,
+      categories,
+      colors,
+    }
+    `
+  )
+
+
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
         {/* Product */}
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
           {/* Product gallery */}
+          <ProductGallery product={product}/>
           {/* Product info */}
+          <ProductInfo product={product}/>
         </div>
       </div>
     </main>

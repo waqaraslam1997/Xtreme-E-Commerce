48ef7ed064381bac86990298d6c649da992b573f
Waqar Aslam <waqar.king10@gmail.com>
2024-06-22 22:24:00 +0500

    remove client token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: "skh44TjY2CTsRDG5b1Y7XOjOEfjWN1kDMpjN4l5rPVOjQZakfeufp1RonBaYI9gmAfhuyBd91YOCJrERvj0epkw284cAjRuLhq4KH0rxxef5GtYIUTihc2wzP3nYwzTNUX4iyeqQJtanDAjl6t9n0hxcdpqB7F66CaJ7uKXyNvO8PeeGSp4B"
 })

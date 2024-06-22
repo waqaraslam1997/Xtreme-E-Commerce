c02f50ded97a655b8a96418d1b3f8d8d7781e5b7
Waqar Aslam <waqar.king10@gmail.com>
2024-06-22 17:56:55 +0500

    Order Summary Completed

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -7,6 +7,13 @@ import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
+
+  const {cartCount, cartDetails, formattedTotalPrice, totalPrice} = useShoppingCart()
+
+  const shippingAmount = cartCount! > 0 ? 500 : 0
+
+  const totalAmount = totalPrice! + shippingAmount
+
   function onCheckout() {}
 
   return (
@@ -21,17 +28,17 @@ export function CartSummary() {
       <dl className="mt-6 space-y-4">
         <div className="flex items-center justify-between">
           <dt className="text-sm">Subtotal</dt>
-          <dd className="text-sm font-medium">Subtotal Amount</dd>
+          <dd className="text-sm font-medium">{formattedTotalPrice}</dd>
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
           <dt className="flex items-center text-sm">
             <span>Shipping estimate</span>
           </dt>
-          <dd className="text-sm font-medium">Shipping Amount</dd>
+          <dd className="text-sm font-medium">{formatCurrencyString({value: shippingAmount, currency: "USD"})}</dd>
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
           <dt className="text-base font-medium">Order total</dt>
-          <dd className="text-base font-medium">Order Amount</dd>
+          <dd className="text-base font-medium">{formatCurrencyString({value: totalAmount, currency: "USD"})}</dd>
         </div>
       </dl>
 
